d58fe4ae07f9b77a6f2d310e806cb760cac94794
mhwwww5 <113410246+mhwwww5@users.noreply.github.com>
2022-09-13 13:49:51 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+    /*모바일메뉴*/
+    $('.mobile_menu>a').on('click', function(){
+        $('.menu_bg').show(); 
+        $('.sidebar_menu').show().animate({
+            right:0
+        });  
+    });
+    $('#header_wrap .close_btn>a').on('click', function(){
+        $('.menu_bg').hide(); 
+        $('.sidebar_menu').animate({
+            right: '-' + 100 + '%'
+                    },function(){
+                        $('.sidebar_menu').hide();          
+                    });  
+    });
+
+});
+
+/*공지 #notice_wrap 닫기*/
+$(function() {  
+    $(".notice .close_btn a").click(function() {
+        $("#notice_wrap").fadeOut();
+    });
+});
